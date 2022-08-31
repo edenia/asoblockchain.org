0188a4f8c7f526296926f08331adecc77e66816a
JustinCast <justincastillovalladares@gmail.com>
2022-08-31 14:06:53 -0600

    delete addresses from the wrong folder & other fixes

--- src/components/FabComponent/index.tsx
+++ src/components/FabComponent/index.tsx
@@ -13,8 +13,6 @@ const FabComponent: React.FC = () => {
 
   return (
     <Link
-      // target='_blank'
-      // href='https://btcnode2.crservers.com/apps/47egazbG6mVHUypcAfMVV67gQSQp/pos'
       href='/pay'
     >
       <Fab
--- src/components/ShowMoreModal/index.tsx
+++ src/components/ShowMoreModal/index.tsx
@@ -8,27 +8,23 @@ type ShowMoreModalType = {
   handleClose: () => void
 }
 
-
-const ShowMoreModal = ({ index, open, handleClose }: ShowMoreModalType) => {
-
-  return (
-    <Dialog
-      open={open}
-      onClose={handleClose}>
-      <DialogContent>
-        <List>
-          {plans[index].items.map((item, idx) => (
-            <div key={idx}>
-              <ListItem style={{ padding: '5px 16px' }}>
-                <Typography variant='subtitle2'> {item}</Typography>
-                <Divider />
-              </ListItem>
-            </div>
-          ))}
-        </List>
-      </DialogContent>
-    </Dialog>
-  )
-}
+const ShowMoreModal = ({ index, open, handleClose }: ShowMoreModalType) => (
+  <Dialog
+    open={open}
+    onClose={handleClose}>
+    <DialogContent>
+      <List>
+        {plans[index].items.map((item, idx) => (
+          <div key={idx}>
+            <ListItem style={{ padding: '5px 16px' }}>
+              <Typography variant='subtitle2'> {item}</Typography>
+              <Divider />
+            </ListItem>
+          </div>
+        ))}
+      </List>
+    </DialogContent>
+  </Dialog>
+)
 
 export default ShowMoreModal
